d825cbe0ba4a69894dbfdfb174b068773b18ed08
lennyi <lenny.ilyashov@wwt.com>
2016-12-06 15:29:57 -0600

    adds index.html to apache profile

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -2,9 +2,30 @@
 class profile::apache {
 
   include apache
-  
+
   service { 'iptables':
     ensure => 'stopped',
     enable => 'false',
+  }
+
+  file { '/var/www/html/index.html':
+    ensure => 'file',
+    group  => '0',
+    mode   => '0644',
+    owner  => '0',
+  }
+
+  file { '/var/www/html/stlpug.jpeg':
+    ensure => 'file',
+    group  => '0',
+    mode   => '0644',
+    owner  => '0',
+  }
+
+  file { '/var/www/html/rambo.jpeg':
+    ensure => 'file',
+    group  => '0',
+    mode   => '0644',
+    owner  => '0',
   }
 }
